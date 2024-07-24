68d3e01b0d345308bfd1bb6c5ff82aa4c20f0420
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-07-24 07:09:33 +0530

    cors fixed

--- index.js
+++ index.js
@@ -14,10 +14,21 @@ const userRoute = require("./routes/userRoutes");
 const app = express();
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
-    origin:['http://localhost:5173', 'http://localhost:5000'],
-    credentials: true
+    origin: 'http://localhost:5174',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
+
+
+app.options('*', cors({
+    origin: 'http://localhost:5174',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
 
 
